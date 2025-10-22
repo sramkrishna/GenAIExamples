691dac5f0521d486c4b183053535b1e234e95825
CICD-at-OPEA <CICD@opea.dev>
2025-10-22 22:31:03 +0000

    Update vLLM version to v0.11.1rc2
    Signed-off-by: CICD-at-OPEA <CICD@opea.dev>

--- .github/env/_build_image.sh
+++ .github/env/_build_image.sh
@@ -1,5 +1,5 @@
 # Copyright (C) 2025 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-export VLLM_VER=v0.11.1rc1
+export VLLM_VER=v0.11.1rc2
 export VLLM_FORK_VER=v0.6.6.post1+Gaudi-1.20.0
